0d1a5b5d8ecf68161e1b9adfbacde941f5215fe5
vpliego <vpliego@smith.edu>
2022-09-19 23:18:37 -0400

    final submission

--- computer.py
+++ computer.py
@@ -1,10 +1,28 @@
 class Computer:
+    # this will probably need to have the date, model, of the computer, etc.
+    #In order to give the main something to call back to, we can define the attributes below and give them the names used in the main. 
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity 
+        self.memory = memory 
+        self.operating_system = operating_system 
+        self.year_made = year_made
+        self.price = price 
 
-    # What attributes will it need?
+# using this method so that we are able to update the price of a computer.
+    def update_price(new_price): 
+        price = new_price
+# this should be able to update the operating_system information. 
+    def update_os(new_os):
+        operating_system = new_os
 
+
+# What attributes will it need?
+     
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    # type = type
+    # string model, string processor, int storage, int space, string interface, int year, int
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,18 @@
 class ResaleShop:
-
+    #here we are defining what the inventory is so that the main has a place to store the computers. 
+    def __init__(self, inventory):
+        self.inventory = inventory 
+      
+      #this should allow computers to be added into the inventory whenever one is bought from another seller.
+    def add_to_inventory():
+        inventory = inventory + 1
+  
     # What attributes will it need?
-
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
     # What methods will you need?
+    
+    #this should allow computers to be removed from the inventory whenever one is sold
+    def remove_from_inventory():
+        inventory = inventory - 1  
+
